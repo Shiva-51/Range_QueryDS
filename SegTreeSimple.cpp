b5a4fe305c32cf8cdf23213a2ea4fd6a6694a4a2
Shiva <shivasharvesh0509@gmail.com>
2025-01-17 20:00:25 +0530

    Update SegTreeSimple.cpp

--- SegTreeSimple.cpp
+++ SegTreeSimple.cpp
@@ -1,9 +1,7 @@
 struct Tree {
-    
     int n;
     vector<int> a;
     vector<int> seg;
-
     Tree(vector<int>&x) {
         a = x;
         n = ssize(a);
@@ -13,11 +11,9 @@ struct Tree {
         }
         seg.resize(4 * sz);
     }
-
     auto merge(auto &l, auto &r) {
         return min(l, r);
     }
-
     void build(int index, int start, int end) {
         if(start == end) {
             seg[index] = a[start];
@@ -28,7 +24,6 @@ struct Tree {
         build(2 * index + 1, mid + 1, end);
         seg[index] = merge(seg[2 * index], seg[2 * index + 1]);
     }
-
     void update(int index, int start, int end, int pt, int val) {
         if(start == end) {
             seg[index] = val;
@@ -43,7 +38,6 @@ struct Tree {
         }
         seg[index] = merge(seg[2 * index], seg[2 * index + 1]);
     }
-
     int query(int index, int start, int end, int qL, int qR) {
         if(start >= qL and end <= qR) {
             return seg[index];
@@ -56,13 +50,10 @@ struct Tree {
         auto ansR = query(2 * index, mid + 1, end, qL, qR);
         return merge(ansL, ansR);
     }
-
     void make_update(int pt, int val) {
         update(1, 0, n-1, pt, val);
     }
-
     int make_query(int l, int r) {
         return query(1, 0, n-1, l, r);
     }
-
 };
